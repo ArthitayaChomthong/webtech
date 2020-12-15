9e41d10594384767877ed5f8309cdd6e81d74910
ArthitayaChomthong <76036837+ArthitayaChomthong@users.noreply.github.com>
2020-12-15 16:04:14 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,5 +6,13 @@
  </head>
  <body>
    <h1>สวัสดีศิลปากร</h1>
+   <h2>Web Technology for Data Science </h2>
+
+   <p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล
+      ชื่อภาษาอังกฤษ : Bachelor of Science Program in Data Science
+   </p>
+
+   <a href="https://cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล</a>
  </body>
 </html>
+
