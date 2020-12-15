f3515cd8b9d04a8f1961616b43b6b1555f1642d2
ArthitayaChomthong <76036837+ArthitayaChomthong@users.noreply.github.com>
2020-12-15 16:16:59 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang = "th">
+ <head>
+  <meta charset = "UTF-8">
+  <title> ทดสอบ HTML </title>
+ </head>
+ <body>
+   <h1>สวัสดีศิลปากร</h1>
+   <h2>Web Technology for Data Science </h2>
+
+   <p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล </p>
+   <p> ชื่อภาษาอังกฤษ : Bachelor of Science Program in Data Science </p>
+   
+   <a href="https://cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล</a>
+   <br>
+   <img src="https://thedatascientist.com/wp-content/uploads/2019/06/what-is-data-science-1024x576.jpg" alt = "Data Science" width = "600" heigh = "400"></a>
+
+ </body>
+</html>
+
